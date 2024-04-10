f3233d4913ebff3717e406db527493c4a7bbf70a
chandru <165871053+kingschandru@users.noreply.github.com>
2024-04-10 10:11:43 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,75 +1,128 @@
-
-CREATE TABLE instructor (
-  Id int(5) primary key,
-  name varchar(15),
-  dept_name varchar(15),
-  salary int(5));
-
-desc instructor;
-
-insert into instructor values
-(10101,'srinivasan','comp.sci',65000),
-(12121,'wu','finance',90000),
-(15151,'mozart','music',40000),
-(22222,'einstein','physics',95000),
-(32343,'el said','history',60000),
-(33456,'gold','physics',87000),
-(45565,'katz','comp.sci',75000),
-(58583,'califieri','history',62000),
-(76543,'singh','finance',80000),
-(76766,'crick','biology',72000),
-(83821,'brandt','comp.sci',92000),
-(98345,'kim','elec.eng',80000);
-
-select * from instructor;
+CREATE TABLE INSTRUCTOR (
+  ID INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  dept_name TEXT NOT NULL,
+  salary INT
+);
 
 
-CREATE TABLE teaches (
-  Id int(5),
-  course_id varchar(15),
-  sec_id int(1),
-  semester varchar(15),
-  year int(4));
-  
-desc teaches;
-
-insert into teaches values
-(10101,'CS-101',1,'fall',2017),
-(10101,'CS-315',1,'spring',2018),
-(10101,'CS-347',1,'fall',2017),
-(12121,'FIN-201',1,'spring',2018),
-(15151,'MU-199',1,'spring',2018),
-(22222,'PHY-101',1,'fall',2017),
-(32343,'HIS-351',1,'spring',2018),
-(45565,'CS-101',1,'spring',2018),
-(45565,'CS-319',1,'spring',2018),
-(76766,'BIO-101',1,'summer',2017),
-(76766,'BIO-301',1,'summer',2018),
-(83821,'CS-190',1,'spring',2017),
-(83821,'CS-190',2,'spring',2017),
-(83821,'CS-319',2,'spring',2018),
-(98345,'EE-181',1,'spring',2017);
+INSERT INTO INSTRUCTOR VALUES (10101, 'Srinivasan', 'Comp. Sci.', '65000');
+INSERT INTO INSTRUCTOR VALUES (12121, 'Wu', 'Finance', '90000');
+INSERT INTO INSTRUCTOR VALUES (15151, 'Mozart', 'Music', '40000');
+INSERT INTO INSTRUCTOR VALUES (22222, 'Einstein', 'Physics', '95000');
+INSERT INTO INSTRUCTOR VALUES (32343, 'El Said', 'History', '60000');
+INSERT INTO INSTRUCTOR VALUES (33456, 'Gold', 'Physics', '87000');
+INSERT INTO INSTRUCTOR VALUES (45565, 'Katz', 'Comp. Sci.', '75000');
+INSERT INTO INSTRUCTOR VALUES (58583, 'Califieri', 'History', '62000');
+INSERT INTO INSTRUCTOR VALUES (76543, 'Singh', 'Finance', '80000');
+INSERT INTO INSTRUCTOR VALUES (76766, 'Crick', 'Biology', '72000');
+INSERT INTO INSTRUCTOR VALUES (83821, 'Brandt', 'Comp. Sci.', '92000');
+INSERT INTO INSTRUCTOR VALUES (98345, 'Kim', 'Elec. Eng.', '80000');
 
 
-select * from teaches;
+SELECT * FROM INSTRUCTOR;
 
-insert into instructor values('10211','Smith','Biology',66000);
-select * from instructor;
 
-delete from instructor where id=10211;
-select * from instructor;
+CREATE TABLE TEACHES (
+  ID INTEGER NOT NULL,
+  COURSE_ID VARCHAR(50) NOT NULL,
+  SEC_ID TEXT NOT NULL,
+  SEMESTER CHAR(50) NOT NULL,
+  YEAR INT NOT NULL,
+  FOREIGN KEY (ID) REFERENCES INSTRUCTOR(ID)
+);
 
-select * from instructor where dept_name='history';
 
-select * from instructor cross join teaches;
+INSERT INTO TEACHES VALUES (10101, 'CS-101', '1', 'Fall', 2017);
+INSERT INTO TEACHES VALUES (10101, 'CS-315', '1', 'Spring', 2018);
+INSERT INTO TEACHES VALUES (10101, 'CS-347', '1', 'Fall', 2017);
+INSERT INTO TEACHES VALUES (12121, 'FIN-201', '1', 'Spring', 2018);
+INSERT INTO TEACHES VALUES (15151, 'MU-199', '1', 'Spring',2018);
+INSERT INTO TEACHES VALUES (22222, 'PHY-101', '1', 'Fall', 2017);
+INSERT INTO TEACHES VALUES (32343, 'HIS-351', '1', 'Spring', 2018);
+INSERT INTO TEACHES VALUES (45565, 'CS-101', '1', 'Spring', 2018);
+INSERT INTO TEACHES VALUES (45565, 'CS-319', '1', 'Spring', 2018);
+INSERT INTO TEACHES VALUES (76766, 'BIO-101', '1', 'Summer', 2017);
+INSERT INTO TEACHES VALUES (76766, 'BIO-301', '1', 'Summer', 2018);
+INSERT INTO TEACHES VALUES (83821, 'CS-190', '2', 'Spring', 2017);
+INSERT INTO TEACHES VALUES (83821, 'CS-319', '2', 'Spring', 2018);
+INSERT INTO TEACHES VALUES (98345, 'EE-181', '1', 'Spring', 2017);
 
-select name from instructor where name like '%wu%';
 
-select name from instructor where salary between 90000 and 100000;
+SELECT * FROM TEACHES;
 
-select * from instructor order by salary;
 
-select course_id from teaches where  semester='fall' and year='2017' or semester='spring' and year='2018';
+INSERT INTO INSTRUCTOR(ID, name, dept_name, salary) VALUES('10211', 'Smith', 'Biology', 66000);
+SELECT * FROM INSTRUCTOR;
+
+
+DELETE FROM INSTRUCTOR WHERE ID="10211";
+SELECT * FROM INSTRUCTOR;
+
+
+SELECT * FROM INSTRUCTOR WHERE dept_name='History';
+
+
+SELECT * FROM INSTRUCTOR CROSS JOIN TEACHES;  
+
+
+SELECT name, COURSE_ID FROM INSTRUCTOR INNER JOIN TEACHES ON INSTRUCTOR.ID=TEACHES.ID;
+
+
+SELECT * From INSTRUCTOR WHERE name LIKE '%in%';
+
+
+SELECT * FROM INSTRUCTOR WHERE salary BETWEEN 90000 and 100000;
+
+
+
+SELECT * FROM INSTRUCTOR order by salary ASC;
+
+
+SELECT COURSE_ID FROM TEACHES where (SEMESTER = "Fall" and YEAR = "2017") or (SEMESTER = "Spring" and YEAR = "2018");
+
+
+SELECT COURSE_ID FROM TEACHES where (SEMESTER = "Fall" and YEAR = "2017") and (SEMESTER = "Spring" and YEAR = "2018");
+
+
+SELECT COURSE_ID FROM TEACHES where (SEMESTER = "Fall" and YEAR = "2017");
+
+
+INSERT INTO INSTRUCTOR(ID, name, dept_name, salary) 
+VALUES('10211', 'Smith', 'Biology', 66000),
+('10212', 'Tom', 'Biology', NULL);
+SELECT * FROM INSTRUCTOR;
+
+
+SELECT * FROM INSTRUCTOR where salary = NULL;
+
+
+SELECT AVG(salary) FROM INSTRUCTOR;
+
+
+
+SELECT COUNT(DISTINCT ID) AS total_instructors_spring_2018 FROM TEACHES WHERE SEMESTER = 'Spring' AND YEAR = 2018;
+
+SELECT COUNT(*) AS number_of_tuples FROM TEACHES;
+
+
+SELECT dept_name, AVG(salary) AS avg_salary FROM INSTRUCTOR GROUP BY dept_name;
+
+
+SELECT dept_name, AVG(salary) AS avg_salary FROM INSTRUCTOR GROUP BY dept_name HAVING avg_salary > 42000;
+
+
+SELECT name FROM INSTRUCTOR WHERE name NOT IN ('Mozart', 'Einstein');
+
+
+SELECT e.name FROM INSTRUCTOR e WHERE e.salary > ANY (SELECT salary FROM INSTRUCTOR WHERE dept_name = 'Biology');
+
+
+SELECT e.name FROM INSTRUCTOR e WHERE e.salary > ALL (SELECT salary FROM INSTRUCTOR WHERE dept_name = 'Biology');
+
+
+SELECT dept_name, AVG(salary) AS avg_salary FROM INSTRUCTOR GROUP BY dept_name HAVING AVG(salary) > 42000;
+
 
 
 
